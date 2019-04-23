8128e0411fb6dfb3d7d5bd22718a1eea547feb47
Alexander King <alexanderddking@gmail.com>
2019-04-24 07:55:01 +0900

    Added icons

---
+++ lib/utils/icons.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+final String playSvg = 'images/play.svg';
+final String pauseSvg = 'images/pause.svg';
+final String recordSvg = 'images/record.svg';
+
+final Widget playIcon = new SvgPicture.asset(
+  playSvg,
+  semanticsLabel: 'Play Icon'
+);
+
+final Widget pauseIcon = new SvgPicture.asset(
+  playSvg,
+  semanticsLabel: 'Play Icon'
+);
+
+final Widget recordIcon = new SvgPicture.asset(
+  playSvg,
+  semanticsLabel: 'Play Icon'
+);
