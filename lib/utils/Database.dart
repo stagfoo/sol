9fe7f2330fd78895748bf6c41460d468a4008e9a
Alex <alex@fs276ed17e.tkyc008.ap.nuro.jp>
2019-04-24 13:53:08 +0900

    File database

---
+++ lib/utils/Database.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+
+Future main() async {
+  Database db = await databaseFactoryIo
+      .openDatabase(join(".dart_tool", "sembast", "example", "record_demo.db"));
+  Store store = db.getStore("my_store");
+  Record record = Record(store, {"name": "ugly"});
+  record = await db.putRecord(record);
+  record = await store.getRecord(record.key);
+  record =
+      (await store.findRecords(Finder(filter: Filter.byKey(record.key)))).first;
+  record = await store.getRecord(record.key);
+  print(record);
+  var records =
+      (await store.findRecords(Finder(filter: Filter.matches("name", "^ugl"))));
+  print(records);
+}
